d1bb7a9cd1336d588a67ed2d2cb4649d1d40c0e6
YASSCODER <mohamedyassine.benaoun@esprit.tn>
2023-04-14 01:40:03 +0100

    fix :  card's image demention

--- src/components/card/card3.jsx
+++ src/components/card/card3.jsx
@@ -4,13 +4,13 @@ const cardThree = () => {
     return(
         <div className="card">
             <div className="card_image">
-                <img src={card3} alt="" />
+                <img className="card-three" src={card3} alt="" />
             </div>
             <div className="card_text">
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut error a architecto iste assumenda repudiandae iure modi voluptatem sed doloribus.</p>
             </div>
             <div className="card_button">
-                <button type="button">List Products</button>
+                <button type="button">Actions</button>
             </div>
         </div>
     );
