d7d21dd77e6dd16b789637512b461f8f1dad7305
YASSCODER <mohamedyassine.benaoun@esprit.tn>
2023-04-14 11:46:59 +0100

    change site language

--- src/components/card/card3.jsx
+++ src/components/card/card3.jsx
@@ -6,8 +6,8 @@ const cardThree = () => {
             <div className="card_image">
                 <img src={card3} alt="" />
             </div>
-            <div className="card_text">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut error a architecto iste assumenda repudiandae iure modi voluptatem sed doloribus.</p>
+            <div className="actions-text">
+                <p>Rester à jour sur l'état de votre Stock</p>
             </div>
             <div className="card_button">
                 <button type="button">Actions</button>
