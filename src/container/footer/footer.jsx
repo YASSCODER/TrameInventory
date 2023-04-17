ee95a7b8e524f195385608ec0ded97035b3a95ea
YASSCODER <mohamedyassine.benaoun@esprit.tn>
2023-04-17 11:27:05 +0100

    add trame logo for the home page and fix the footer content

--- src/container/footer/footer.jsx
+++ src/container/footer/footer.jsx
@@ -1,19 +1,17 @@
 import './footer.css';
-import logo from '../../assets/logoHR.svg'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBookBookmark} from '@fortawesome/free-solid-svg-icons'
+import weDev from '../../assets/logoHR.svg'
+import trame from '../../assets/LOGOTRAME.svg'
 const Footer = () => {
     return(
         <div className="footer_container">
-            <div className="client">
-                <p>TramePub</p>
-            </div>
-            <div className="divider">
-            </div>
-            <div className="catalog_link">
-                <a href="#">www.Catalog.com</a>
+            <div className="studio">
+                <a href="#"><img src={weDev} alt="" /></a>
             </div>
             <div className="divider"></div>
-            <div className="studio">
-                <img src={logo} alt="" />
+            <div className="catalog_link">
+                <a href="https://trame.hideagifts.com/fr/catalogue/"><FontAwesomeIcon icon ={faBookBookmark} className='icon'/></a>
             </div>
         </div>
     )
